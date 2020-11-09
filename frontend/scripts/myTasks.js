49bb8e6e27a7f52cd05251a3dead57e0dbbce01c
DaryaDan <darya.danchenko1999@gmail.com>
2020-11-09 14:48:45 +0300

    added: task execution line

---
+++ frontend/scripts/myTasks.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $('progress').after('<div style="position: absolute; margin-top: -37px; text-align: center; width: 400px; font-size: 0.8em">Вопрос 1 из 7 (12%)<div>');
+})
