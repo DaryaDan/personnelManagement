e0eebb3c03583e985bf2baf6a96405c00c2097a8
DaryaDan <darya.danchenko1999@gmail.com>
2020-11-10 20:12:53 +0300

    pagee "my tasks" was made

--- frontend/scripts/myTasks.js
+++ frontend/scripts/myTasks.js
@@ -1,3 +1,61 @@
-$(document).ready(function() {
-  $('progress').after('<div style="position: absolute; margin-top: -37px; text-align: center; width: 400px; font-size: 0.8em">Вопрос 1 из 7 (12%)<div>');
-})
+(function allTasks() {
+let urlMain=`http://localhost:3000/tasks`;
+let data = GetAll(urlMain).then(result =>myAlltasks(result));
+})();
+
+function myAlltasks(result){
+  let length=result.length;
+  let strNew = ' ';
+  let strInProcess = ' ';
+  let strComplete = ' ';
+  let taskNew = document.getElementById('tasksNew');
+  let taskInProcess = document.getElementById('tasksInProcess');
+  let taskComplete = document.getElementById('tasksComplete');
+for(let i=0;i<length;i++){
+  if(result[i].manID===Number(localStorage.personID)){
+
+let subLength=result[i].subtasks.length;
+let subLengthChange;
+if(subLength===0){subLengthChange=1;}
+else {subLengthChange=subLength;}
+
+let compliteLength=0;
+for(let k=0;k<subLength;k++){
+  console.log(result[i].subtasks[k][1]);
+if(result[i].subtasks[k][1]==='true'){
+  compliteLength++;
+}
+}
+
+let percent=" ";
+percent+=((Number(compliteLength)/subLengthChange)*100).toFixed(2);
+percent+="%"
+if(Number(compliteLength)===0){
+strNew += `
+<div>
+<div>${result[i].taskName}${percent}
+<progress id="progressbar" value=${compliteLength} max=${subLength}></progress>
+    </div>`
+  }
+
+  else if(Number(compliteLength)<subLengthChange){
+  strInProcess += `
+  <div>
+  <div>${result[i].taskName}${percent}
+  <progress id="progressbar" value=${compliteLength} max=${subLength}></progress>
+      </div>`
+    }
+
+    else if(Number(compliteLength)===subLengthChange){
+    strComplete += `
+    <div>
+    <div>${result[i].taskName}${percent}
+    <progress id="progressbar" value=${compliteLength} max=${subLength}></progress>
+        </div>`
+      }
+
+}}
+taskNew.innerHTML = strNew;
+taskInProcess.innerHTML = strInProcess;
+taskComplete.innerHTML = strComplete;
+}
