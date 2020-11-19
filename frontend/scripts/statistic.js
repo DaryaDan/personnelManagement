fe1ee369e7c371b2a3ce2ccfff26ddd9d476fdf4
DaryaDan <darya.danchenko1999@gmail.com>
2020-11-19 14:24:37 +0300

    statistic in process

---
+++ frontend/scripts/statistic.js
@@ -0,0 +1,24 @@
+new Chart(document.getElementById("line-chart"), {
+  type: 'line',
+  data: {
+    labels: ["Январь","Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+    datasets: [{
+        data: [86,114,106,106,107,111,133,221,783,2478],
+        label: "Africa",
+        borderColor: "#3e95cd",
+        fill: false
+      }, {
+        data: [282,350,411,502,635,809,947,1402,3700,5267],
+        label: "Asia",
+        borderColor: "#8e5ea2",
+        fill: false
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Общее число задач по месяцам и мои задачи'
+    }
+  }
+});
