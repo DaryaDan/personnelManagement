c31410b3265fc10d10c47ff3730f07d575200768
DaryaDan <darya.danchenko1999@gmail.com>
2020-11-19 20:31:08 +0300

    finalized statistics

--- frontend/scripts/statistic.js
+++ frontend/scripts/statistic.js
@@ -1,15 +1,40 @@
+(function stat(){
+  let urlMain=`http://localhost:3000/tasks`;
+let data = GetAll(urlMain).then(result => drawCharts(result));
+})();
+
+function drawCharts(result){
+let length=result.length;
+let allTasks=[];
+let allTask=0;
+let myTasks=[];
+let myTask=0;
+for(let k=1;k<=12;k++){
+for(let i=0;i<length;i++){
+  if (k===Number(result[i].term.slice(5, 7))){
+    allTask++;
+if(result[i].manID===Number(localStorage.personID)){
+  myTask++;
+};
+};
+};
+allTasks.push(allTask);
+myTasks.push(myTask);
+allTask=0;
+myTask=0;
+};
 new Chart(document.getElementById("line-chart"), {
   type: 'line',
   data: {
     labels: ["Январь","Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
     datasets: [{
-        data: [86,114,106,106,107,111,133,221,783,2478],
-        label: "Africa",
+        data: allTasks,
+        label: "Общее число задач",
         borderColor: "#3e95cd",
         fill: false
       }, {
-        data: [282,350,411,502,635,809,947,1402,3700,5267],
-        label: "Asia",
+        data: myTasks,
+        label: "Мои задачи",
         borderColor: "#8e5ea2",
         fill: false
       }
@@ -18,7 +43,96 @@ new Chart(document.getElementById("line-chart"), {
   options: {
     title: {
       display: true,
-      text: 'Общее число задач по месяцам и мои задачи'
+      text: 'Годовой график задач'
     }
   }
 });
+
+let pieMy=[0,0,0];
+let pieAll=[0,0,0];
+var date = new Date();
+let month=date.getMonth();
+month++;
+
+for(let i=0;i<length;i++){ //перебор всех задач
+  if (month===Number(result[i].term.slice(5, 7))){ //если месяц совпал
+let subLength=result[i].subtasks.length;  //длина подзадач совпавшей задачи
+let a=0,b=0;
+for(let k=0;k<subLength;k++){  //перебор подзадач задачи
+if(result[i].subtasks[k][1]==="true"){  //если она выполнена
+a++;
+}
+else {  //иначе
+  b++;
+}
+}
+if(a===0){   //записываем в наш массив
+  pieAll[2]++;
+}
+else if(b===0){
+  pieAll[0]++;
+}
+else {
+  pieAll[1]++;
+}
+
+if(result[i].manID===Number(localStorage.personID)){ //для нашего пользователя
+  let aa=0,bb=0;
+  for(let k=0;k<subLength;k++){  //перебор подзадач задачи
+  if(result[i].subtasks[k][1]==="true"){  //если она выполнена
+  aa++;
+  }
+  else {  //иначе
+    bb++;
+  }
+  }
+  if(a===0){
+    pieMy[2]++;
+  }
+  else if(b===0){
+    pieMy[0]++;
+  }
+  else {
+    pieMy[1]++;
+  }
+};
+
+};
+};
+new Chart(document.getElementById("pieChart1"), {
+    type: 'pie',
+    data: {
+      labels: ["Новые", "В процессе", "Завершенные"],
+      datasets: [{
+        label: "задач",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#c45850"],
+        data: pieAll
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Общая статистика за месяц'
+      }
+    }
+});
+
+new Chart(document.getElementById("pieChart2"), {
+    type: 'pie',
+    data: {
+      labels: ["Новые", "В процессе", "Завершенные"],
+      datasets: [{
+        label: "задач",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#c45850"],
+        data: pieMy
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Моя статистика за месяц'
+      }
+    }
+});
+
+}
