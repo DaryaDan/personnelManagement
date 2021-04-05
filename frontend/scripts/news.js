c34a85bf075c3f3b2ab0aab072f46cba42e3c5fb
DaryaDan <darya.danchenko1999@gmail.com>
2021-04-05 14:25:00 +0300

    update styles

--- frontend/scripts/news.js
+++ frontend/scripts/news.js
@@ -1,11 +1,11 @@
 function newNews(){
-      let url=`http://localhost:3000/news`;
-      let logElem = document.querySelector(".stage1");
-let newsID=Math.floor(Math.random()*Math.floor(100));
-let news=document.getElementById('Field2').value;
-let type=document.getElementById('Field1').value;
-let now = new Date().toLocaleString();
-if(news.length!==0&&type.length!==0){
+  let url=`http://localhost:3000/news`;
+  let logElem = document.querySelector(".stage1");
+  let newsID=Math.floor(Math.random()*Math.floor(100));
+  let news=document.getElementById('Field2').value;
+  let type=document.getElementById('Field1').value;
+  let now = new Date().toLocaleString();
+  if(news.length!==0&&type.length!==0){
     const body = {
       newsID: `${newsID}`,
       authorName: localStorage.name,
@@ -15,7 +15,7 @@ if(news.length!==0&&type.length!==0){
     };
     let dataAll = AddOne(url,body).then(result =>console.log(result));
     logElem.textContent = "Успешно";
-    }
+  }
   else {
     logElem.textContent = "Ошибка";
   }
